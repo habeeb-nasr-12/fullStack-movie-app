9d7571b2bbce801b8647679defe798d67b0ac1b8
Habeeb <habebenasr4@gmail.com>
2025-07-27 22:41:28 +0300

    chore:  clean up ui index exports from old component

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,4 +1,3 @@
 export { default as Loading } from './Loading';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as ProtectedRoute } from './ProtectedRoute';
-export { default as PerformanceMonitor } from './PerformanceMonitor'; 
