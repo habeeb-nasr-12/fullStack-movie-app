685f4fec9c326d9f95ec85d0d508f92252a3bcc8
Habeeb <habebenasr4@gmail.com>
2025-07-27 22:19:54 +0300

    refactor: simplify production configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-const DbURL = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 const config = {
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
@@ -27,31 +26,7 @@ const config = {
     idle: 10000,
   },
 };
-
-const ProductionConfig = {
-  url: DbURL,
-  dialect: 'mysql',
-  logging: false,
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  pool: {
-    max: 20,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
-
 export default {
   development: config,
-  test: config,
-  production: ProductionConfig,
+  production: config,
 }; 
