fc41177a5d23b8f352a4e797458b2d622be47217
Habeeb <habebenasr4@gmail.com>
2025-07-29 23:48:04 +0300

    refactor: remove unused MovieTVShow and User model files to streamline model management

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,15 +1,10 @@
 import { sequelize } from '../../config/database.js';
 import { Sequelize } from 'sequelize';
-
-// Import model definitions
 import createMovieTVShow from './MovieTVShow.model.js';
 import createUser from './User.model.js';
-
-// Initialize models
 const MovieTVShow = createMovieTVShow(sequelize, Sequelize.DataTypes);
 const User = createUser(sequelize, Sequelize.DataTypes);
 
-// Create database object
 const db = {
   MovieTVShow,
   User,
